935989a7bec09a738082d669376fadf270b3b9d9
LostGhostTown <20000125ysh@gmail.com>
2022-09-25 17:54:35 -0700

    Welcome page (first draft

---
+++ MyApplication55/app/src/main/java/com/example/myapplication55/SignUpPage.java
@@ -0,0 +1,13 @@
+package com.example.myapplication55;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+}
