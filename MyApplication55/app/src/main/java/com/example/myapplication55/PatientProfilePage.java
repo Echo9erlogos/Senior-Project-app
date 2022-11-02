25a74bb2b389c35f72309bdae53791fba3559b85
LostGhostTown <20000125ysh@gmail.com>
2022-11-01 17:38:16 -0700

    Two versions of the profile page

---
+++ MyApplication55/app/src/main/java/com/example/myapplication55/PatientProfilePage.java
@@ -0,0 +1,22 @@
+package com.example.myapplication55;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class PatientProfilePage extends AppCompatActivity {
+    Button button=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_profile_page);
+        button=(Button)findViewById(R.id.edit);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setClass(PatientProfilePage.this,EditPatientProfilePage.class);
+            startActivity(intent);
+        });
+    }
+}
