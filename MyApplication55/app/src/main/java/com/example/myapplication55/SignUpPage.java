b17d3627b16d63f25de2ae088aa2fc911bec71ea
Abraham Mendoza <abraham.mendoza@live.com>
2022-11-01 17:59:13 -0700

    moved register
    register moved to the correct file path and now links to the rest to the app

--- MyApplication55/app/src/main/java/com/example/myapplication55/SignUpPage.java
+++ MyApplication55/app/src/main/java/com/example/myapplication55/SignUpPage.java
@@ -8,6 +8,6 @@ public class SignUpPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_page);
+        setContentView(R.layout.activity_register);
     }
 }
