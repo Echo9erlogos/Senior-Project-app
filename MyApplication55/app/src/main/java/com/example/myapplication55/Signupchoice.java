0290fdaca7043389d232f37e396fd52c718fabd2
Echo9erlogos <andres00espar@gmail.com>
2022-11-09 11:26:43 -0800

    worked on sign up choice page
    added code but for the java part i commented everything cause it wasn't working for some reason

--- MyApplication55/app/src/main/java/com/example/myapplication55/Signupchoice.java
+++ MyApplication55/app/src/main/java/com/example/myapplication55/Signupchoice.java
@@ -2,7 +2,11 @@ package com.example.myapplication55;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Signupchoice extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class Signupchoice extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signupchoice);
+
+         //class OnClick implements View.OnClickListener {
+           // @Override
+            //public void onClick(View v) {
+                //Intent intent = null;
+                //switch (v.getTag().toString()) {
+                   // case "patient":
+                        //intent = new Intent(Signupchoice.this, SignUpPage.class);
+                        //break;
+                    //case "therapist":
+                        //intent = new Intent(Signupchoice.this, TherapistSignup.class);
+                        //break;
+                    //default:
+                        //break;
+                //}
+                //if (intent != null) {
+                    //startActivity(intent);
+                //} else {
+                    //Toast.makeText(Signupchoice.this, "invalid button", Toast.LENGTH_SHORT).show();
+                //}
+            //}
+        //}
     }
 }
