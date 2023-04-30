fb34fe03b192a9ed7f19a9a308367f329f0a812a
LostGhostTown <20000125ysh@gmail.com>
2023-04-29 21:45:10 -0700

    home button

--- MyApplication55/app/src/main/java/com/example/myapplication55/Patient_StatusPage_Activity.java
+++ MyApplication55/app/src/main/java/com/example/myapplication55/Patient_StatusPage_Activity.java
@@ -2,12 +2,16 @@ package com.example.myapplication55;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Patient_StatusPage_Activity extends AppCompatActivity {
     TextView week;
     TextView advice;
+    ImageButton home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,14 @@ public class Patient_StatusPage_Activity extends AppCompatActivity {
         String advices=getIntent().getStringExtra("advices");
         week.setText(weeks);
         advice.setText(advices);
+        home=findViewById(R.id.backhome);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent();
+                intent.setClass(Patient_StatusPage_Activity.this,Patient_HomePage_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
